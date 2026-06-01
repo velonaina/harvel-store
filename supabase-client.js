1e14bc6025ed21ec835b87986e92cc577062f0ef
velonaina <47855920+velonaina@users.noreply.github.com>
2026-06-02 00:28:48 +0300

    Update supabase-client.js

--- supabase-client.js
+++ supabase-client.js
@@ -1,1 +1,8 @@
-import('./produits-supabase.js').then(m => m.getProduitsActifs()).then(console.log)
+// supabase-client.js — NE PAS commiter sur GitHub (ajouter au .gitignore)
+
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
+
+const SUPABASE_URL  = 'https://rcikcebyewurgjhnqeto.supabase.co'
+const SUPABASE_KEY  = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJjaWtjZWJ5ZXd1cmdqaG5xZXRvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAzMTkxNTMsImV4cCI6MjA5NTg5NTE1M30.niZWrq2AOY6xRjI9Do-wfgmKDqVIr1GazDEW3cgq7Po'
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
